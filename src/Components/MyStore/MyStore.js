a37bd43a80b918d48602b5027ba043f040b11831
ijTuhin <isratishu0209@gmail.com>
2022-03-26 01:13:21 +0600

    cart & product section created

--- src/Components/MyStore/MyStore.js
+++ src/Components/MyStore/MyStore.js
@@ -11,6 +11,24 @@ const MyStore = () => {
                     <a href="/more">More</a>
                 </div>
             </nav>
+
+            <main>
+                <div className='row'>
+                    <div className="product col-9 border">
+                        <h2>Products to be shown</h2>
+                        <div className="row">
+                            <div className="col-md-4 col-sm-12">something</div>
+                            <div className="col-md-4 col-sm-12">something</div>
+                            <div className="col-md-4 col-sm-12">something</div>
+                            <div className="col-md-4 col-sm-12">something</div>
+                            <div className="col-md-4 col-sm-12">something</div>
+                        </div>
+                    </div>
+                    <div className="cart col-3 border">
+                        <h2>Cart will be stored here</h2>
+                    </div>
+                </div>
+            </main>
         </div>
     );
 };
