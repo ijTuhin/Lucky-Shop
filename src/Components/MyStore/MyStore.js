b9fce83d4bbb36a4c224f921e50f1f20a3265b17
ijTuhin <isratishu0209@gmail.com>
2022-03-26 00:39:53 +0600

    Getting started

---
+++ src/Components/MyStore/MyStore.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './MyStore.css'
+const MyStore = () => {
+    return (
+        <div>
+            <h3>Getting started</h3>
+        </div>
+    );
+};
+
+export default MyStore;
